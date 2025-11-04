fedfd67f35174e52dbb19dbe0ee765c50fc751c1
Paulino Flores Melo <PaulinoFloresMelo@users.noreply.github.com>
2025-11-04 17:22:11 -0600

    add service pagination

--- src/app/store-front/pages/category-page/category-page.component.ts
+++ src/app/store-front/pages/category-page/category-page.component.ts
@@ -5,24 +5,31 @@ import { map } from 'rxjs';
 
 import { TermCardComponent } from "@/terms/components/term-card/term-card.component";
 import { TermsService } from '@/terms/services/terms.service';
+import { PaginationComponent } from "@/shared/components/pagination/pagination.component";
+import { PaginationService } from '@/shared/components/pagination/pagination.service';
 
 @Component({
   selector: 'app-category-page',
-  imports: [TermCardComponent],
+  imports: [TermCardComponent, PaginationComponent],
   templateUrl: './category-page.component.html',
 })
 export class CategoryPageComponent {
   
   route  = inject(ActivatedRoute);
   termsService = inject(TermsService)
-  
+  paginationService = inject(PaginationService)
+
   category = toSignal(this.route.params.pipe(map(( { category }) => category )));
   
   termsResource = rxResource({
-    params: () => ({ category: this.category() }),
+    params: () => ({ 
+      category: this.category(),
+      page: this.paginationService.currentPage() -1  
+    }),
     stream: ({ params }) => {
       return this.termsService.getTerms({
         category: params.category,
+        offset: params.page * 9,
       });
     }
   })
--- src/app/store-front/pages/home-page/home-page.component.ts
+++ src/app/store-front/pages/home-page/home-page.component.ts
@@ -2,23 +2,26 @@ import { Component, inject } from '@angular/core';
 import { TermCardComponent } from '@/terms/components/term-card/term-card.component';
 import { TermsService } from '@/terms/services/terms.service';
 import { rxResource } from '@angular/core/rxjs-interop';
+import { PaginationComponent } from '@/shared/components/pagination/pagination.component';
+import { PaginationService } from '@/shared/components/pagination/pagination.service';
+
 
 @Component({
   selector: 'app-home-page',
-  imports: [TermCardComponent],
+  imports: [TermCardComponent, PaginationComponent],
   templateUrl: './home-page.component.html',
 })
 export class HomePageComponent {
-  termsService = inject(TermsService)
+  termsService      = inject(TermsService);
+  paginationService = inject(PaginationService)
   
   termsResource = rxResource({
-    params: () => ({}),
+    params: () => ({ page: this.paginationService.currentPage() - 1 }),
     stream: ({ params }) => {
       return this.termsService.getTerms({
-        limit: 1,
-        category: 'cocina',
+        offset: params.page * 9,
       });
     }
   })
 
- }
+}
