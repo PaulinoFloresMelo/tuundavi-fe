906f0fe6a3d66e74a405bdcb4cdb230f07daf29e
Paulino Flores Melo <PaulinoFloresMelo@users.noreply.github.com>
2025-11-04 13:20:26 -0600

    add category page

--- src/app/store-front/pages/category-page/category-page.component.ts
+++ src/app/store-front/pages/category-page/category-page.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
+
+import { TermCardComponent } from "@/terms/components/term-card/term-card.component";
+import { TermsService } from '@/terms/services/terms.service';
 
 @Component({
   selector: 'app-category-page',
-  imports: [],
+  imports: [TermCardComponent],
   templateUrl: './category-page.component.html',
 })
-export class CategoryPageComponent { }
+export class CategoryPageComponent {
+  
+  route  = inject(ActivatedRoute);
+  termsService = inject(TermsService)
+  
+  category = toSignal(this.route.params.pipe(map(( { category }) => category )));
+  
+  termsResource = rxResource({
+    params: () => ({ category: this.category() }),
+    stream: ({ params }) => {
+      return this.termsService.getTerms({
+        category: params.category,
+      });
+    }
+  })
+}
