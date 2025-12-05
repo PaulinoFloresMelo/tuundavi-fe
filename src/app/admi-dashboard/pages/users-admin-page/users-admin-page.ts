3b2bdf94a0ba456337401f36d915f86cb5e05764
Paulino Flores Melo <PaulinoFloresMelo@users.noreply.github.com>
2025-12-05 11:16:50 -0600

    add user/s admin page and edit home page and admin page

---
+++ src/app/admi-dashboard/pages/users-admin-page/users-admin-page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-admin-page',
+  imports: [],
+  templateUrl: './users-admin-page.html',
+})
+export class UsersAdminPage { }
--- src/app/store-front/component/front-navbar/front-navbar.component.ts
+++ src/app/store-front/component/front-navbar/front-navbar.component.ts
@@ -1,12 +1,31 @@
 import { Component, inject } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { AuthService } from '../../../auth/services/auth.service';
 
 @Component({
   selector: 'front-navbar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [ RouterOutlet,RouterLink, RouterLinkActive],
   templateUrl: './front-navbar.component.html',
 })
 export class FrontNavbarComponent { 
   authService = inject(AuthService);
+
+  semantics = [
+    "Adverbio",
+    "Animales",
+    "Colores",
+    "Dias de la semana",
+    "Verbos Frecuentes",
+    "Numeros 1-100",
+    "Objetos escolares",
+    "Agricultura",
+    "Estados de ánimo",
+    "Familiares y gente",
+    "Frutas y vegetales",
+    "Objetos domesticos",
+    "Pronombre personales",
+    "Clima y estaciones del año",
+    "Comidas y bebidas",
+  ]
+
 }
