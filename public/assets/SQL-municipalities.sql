ee1f5d85f2f215d984fc3b7a0ec76bba1cea62ee
Paulino Flores Melo <PaulinoFloresMelo@users.noreply.github.com>
2026-08-18 20:53:36 -0600

    working in new changes3

---
+++ public/assets/SQL-municipalities.sql
@@ -0,0 +1,72 @@
+-- Municipios de Puebla que hablan Náhuatl de la Sierra, noreste de Puebla.
+INSERT INTO municipalities ( id, name, state_id ) VALUES
+  ("1-21PL-1An", "Atempan", "21PL"),
+  ("1-21PL-2Ao", "Ayotoxco de Guerrero", "21PL"),
+  ("1-21PL-3Cn", "Cuautempan", "21PL"),
+  ("1-21PL-4Co", "Cuetzalan del Progreso", "21PL"),
+  ("1-21PL-5Ca", "Chignautla", "21PL"),
+  ("1-21PL-6Hn", "Hueyapan", "21PL"),
+  ("1-21PL-7Ho", "Hueytamalco", "21PL"),
+  ("1-21PL-8Hn", "Huitzilán de Serdán", "21PL"),
+  ("1-21PL-9In", "Ixtacamaxtitlán", "21PL"),
+  ("1-21PL-10Ja", "Jonotla", "21PL"),
+  ("1-21PL-11Na", "Nauzontla", "21PL"),
+  ("1-21PL-12To", "Tenampulco", "21PL"),
+  ("1-21PL-13To", "Tetela de Ocampo", "21PL"),
+  ("1-21PL-14Tn", "Teziutlán", "21PL"),
+  ("1-21PL-15Tc", "Tlatlauquitepec", "21PL"),
+  ("1-21PL-16Ta", "Tuzamapan de Galeana", "21PL"),
+  ("1-21PL-17Xo", "Xiutetelco", "21PL"),
+  ("1-21PL-18Xo", "Xochiapulco", "21PL"),
+  ("1-21PL-19Xz", "Xochitlán de Vicente Suárez", "21PL"),
+  ("1-21PL-20Yc", "Yaonáhuac", "21PL"),
+  ("1-21PL-21Za", "Zacapoaxtla", "21PL"),
+  ("1-21PL-22Za", "Zautla", "21PL"),
+  ("1-21PL-23Zz", "Zapotitlán de Méndez", "21PL"),
+  ("1-21PL-24Za", "Zaragoza", "21PL"),
+  ("1-21PL-25Zn", "Zoquiapan", "21PL");
+
+-- Municipios de Puebla que hablan Náhuatl del noroeste central.
+INSERT INTO municipalities ( id, name, state_id ) VALUES
+  ("2-21PL-1Ca", "Chiconcuautla", "21PL" ),
+  ("2-21PL-2Hy", "Honey", "21PL" ),
+  ("2-21PL-3Ho", "Huauchinango", "21PL" ),
+  ("2-21PL-4Ja", "Jopala", "21PL" ),
+  ("2-21PL-5Jo", "Juan Galindo", "21PL" ),
+  ("2-21PL-6Nn", "Naupan", "21PL" ),
+  ("2-21PL-7Pn", "Pahuatlán", "21PL" ),
+  ("2-21PL-8Ta", "Tlaola", "21PL" ),
+  ("2-21PL-9Ta", "Tlapacoya", "21PL" ),
+  ("2-21PL-10Xc", "Xicotepec", "21PL" ),
+  ("2-21PL-11Za", "Zihuateutla", "21PL" );
+
+-- Municipios de Veracruz que hablan Náhuatl del Istmo.
+INSERT INTO municipalities ( id, name, state_id ) VALUES 
+  ("3-30VZ-1Ce", "Cosoleacaque", "30VZ"),
+  ("3-30VZ-2Mn", "Mecayapan", "30VZ"),
+  ("3-30VZ-3Pn", "Pajapan", "30VZ"),
+  ("3-30VZ-4Za", "Zaragoza", "30VZ");
+
+-- Municipios de Veracruz que hablan Náhuatl de la Huasteca veracruzana.
+INSERT INTO municipalities ( id, name, state_id ) VALUES
+("4-30VZ-1Bz", "Benito Juárez", "30VZ"),
+("4-30VZ-2Co", "Castillo de Teayo", "30VZ"),
+("4-30VZ-3Ca", "Chalma", "30VZ"),
+("4-30VZ-4Cl", "Chiconamel", "30VZ"),
+("4-30VZ-5Cc", "Chicontepec", "30VZ"),
+("4-30VZ-6Ca", "Chontla", "30VZ"),
+("4-30VZ-7Cl", "Citlaltépetl", "30VZ"),
+("4-30VZ-8Ha", "Huayacocotla", "30VZ"),
+("4-30VZ-9In", "Ilamatlán", "30VZ"),
+("4-30VZ-10Ic", "Ixcatepec", "30VZ"),
+("4-30VZ-11Io", "Ixhuatlán de Madero", "30VZ"),
+("4-30VZ-12Po", "Pánuco", "30VZ"),
+("4-30VZ-13Pz", "Platón Sánchez", "30VZ"),
+("4-30VZ-14Tn", "Tamalín", "30VZ"),
+("4-30VZ-15Ta", "Tantima", "30VZ"),
+("4-30VZ-16Ta", "Tantoyuca", "30VZ"),
+("4-30VZ-17Ta", "Tepetzintla", "30VZ"),
+("4-30VZ-18Tc", "Texcatepec", "30VZ"),
+("4-30VZ-19To", "Tlachichilco", "30VZ"),
+("4-30VZ-20Te", "Temapache", "30VZ"),
+("4-30VZ-21Zs", "Zontecomatlán de López y Fuentes", "30VZ");
